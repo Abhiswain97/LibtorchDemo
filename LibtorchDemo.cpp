9d54d8dbc6a17109f4d658eb3374f1d52ae4c41e
Abhiswain97 <abhiswain.it.2016@gmail.com>
2021-04-01 21:13:41 +0530

    first commit

---
+++ LibtorchDemo.cpp
@@ -0,0 +1,14 @@
+﻿// LibtorchDemo.cpp : Defines the entry point for the application.
+//
+
+#include "LibtorchDemo.h"
+
+#include <torch/torch.h>
+
+using namespace std;
+
+int main()
+{
+	cout << torch::randn({3, 2});
+	return 0;
+}
