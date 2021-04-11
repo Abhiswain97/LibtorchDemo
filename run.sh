22d9990a61885662bb4c1a5f0e21ba3e72b0a4cc
Abhiswain97 <54038552+Abhiswain97@users.noreply.github.com>
2021-04-11 19:09:40 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 if [ -d "./build" ]; then
 	echo "Build folder exists, rebuilding and executing"
 	cd build
-	cmake -DCMAKE_PREFIX_PATH=$1
+	cmake -DCMAKE_PREFIX_PATH=$1 ..
 	make
 	./LibtorchDemo 
 else
 	echo "Creating build folder and building the project"
 	mkdir build
 	cd build
-	cmake -DCMAKE_PREFIX_PATH=$1
+	cmake -DCMAKE_PREFIX_PATH=$1 ..
 	make 
 	./LibtorchDemo
 fi
