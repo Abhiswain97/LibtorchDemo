4df91466fc4d92fc0d8e17a3b1068e9b3c04100b
Abhishek <abhiswain.it.2016@gmail.com>
2021-04-18 12:24:38 +0530

    remove progress bar

--- LibtorchDemo.cpp
+++ LibtorchDemo.cpp
@@ -7,29 +7,6 @@ int main()
 
 	std::cout << "Checking for cuda avalability: " << std::endl;
 
-	float progress = 0.0;
-	while (progress <= 1.0)
-	{
-		int barWidth = 70;
-
-		std::cout << "[";
-		int pos = barWidth * progress;
-		for (int i = 0; i <= barWidth; ++i)
-		{
-			if (i < pos)
-				std::cout << "=";
-			else if (i == pos)
-				std::cout << ">";
-			else
-				std::cout << " ";
-		}
-		std::cout << "] " << int(progress * 100.0) << " %\r";
-		std::cout.flush();
-
-		progress += 0.01; // for demonstration only
-	}
-	std::cout << std::endl;
-
 	if (torch::cuda::is_available())
 	{
 		std::cout << "cuda is available!" << std::endl;
